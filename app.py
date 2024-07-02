41239165aa159fa5ea200558923add62eb81fdd4
Gonza Sanabria <gsyanke@gmail.com>
2024-07-01 22:22:23 -0300

    cambios rama Gonza

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class Catalogo:
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS productos (
             codigo INT AUTO_INCREMENT PRIMARY KEY,
             descripcion VARCHAR(255) NOT NULL,
-            cantidad INT NOT NULL,
+            Color VARCHAR(255) NOT NULL,
             precio DECIMAL(10, 2) NOT NULL,
             imagen_url VARCHAR(255),
             proveedor INT(4))''')
@@ -61,10 +61,10 @@ class Catalogo:
         self.cursor = self.conn.cursor(dictionary=True)
         
     #----------------------------------------------------------------
-    def agregar_producto(self, descripcion, cantidad, precio, imagen, proveedor):
+    def agregar_producto(self, descripcion, Color, precio, imagen, proveedor):
                
-        sql = "INSERT INTO productos (descripcion, cantidad, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s)"
-        valores = (descripcion, cantidad, precio, imagen, proveedor)
+        sql = "INSERT INTO productos (descripcion, Color, precio, imagen_url, proveedor) VALUES (%s, %s, %s, %s, %s)"
+        valores = (descripcion, Color, precio, imagen, proveedor)
 
         self.cursor.execute(sql, valores)        
         self.conn.commit()
@@ -77,9 +77,9 @@ class Catalogo:
         return self.cursor.fetchone()
 
     #----------------------------------------------------------------
-    def modificar_producto(self, codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor):
-        sql = "UPDATE productos SET descripcion = %s, cantidad = %s, precio = %s, imagen_url = %s, proveedor = %s WHERE codigo = %s"
-        valores = (nueva_descripcion, nueva_cantidad, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
+    def modificar_producto(self, codigo, nueva_descripcion, nueva_Color, nuevo_precio, nueva_imagen, nuevo_proveedor):
+        sql = "UPDATE productos SET descripcion = %s, Color = %s, precio = %s, imagen_url = %s, proveedor = %s WHERE codigo = %s"
+        valores = (nueva_descripcion, nueva_Color, nuevo_precio, nueva_imagen, nuevo_proveedor, codigo)
         self.cursor.execute(sql, valores)
         self.conn.commit()
         return self.cursor.rowcount > 0
@@ -105,7 +105,7 @@ class Catalogo:
             print("-" * 40)
             print(f"Código.....: {producto['codigo']}")
             print(f"Descripción: {producto['descripcion']}")
-            print(f"Cantidad...: {producto['cantidad']}")
+            print(f"Color...: {producto['Color']}")
             print(f"Precio.....: {producto['precio']}")
             print(f"Imagen.....: {producto['imagen_url']}")
             print(f"Proveedor..: {producto['proveedor']}")
@@ -118,15 +118,15 @@ class Catalogo:
 # Cuerpo del programa
 #--------------------------------------------------------------------
 # Crear una instancia de la clase Catalogo
-""" catalogo = Catalogo(host='localhost', user='root', password='lospokes1', database='miapp') """
-catalogo = Catalogo(host='lospokes.mysql.pythonanywhere-services.com', user='lospokes', password='uHrDCGg85@Yq886', database='lospokes$miapp')
+""" catalogo = Catalogo(host='localhost', user='root', password='gonzasana1', database='miapp') """
+catalogo = Catalogo(host='gonzasana.mysql.pythonanywhere-services.com', user='gonzasana', password='uHrDCGg85@Yq886', database='gonzasana$miapp')
 
 
 # Carpeta para guardar las imagenes.
 """ RUTA_DESTINO = './static/imagenes/' """
 
 #Al subir al servidor, deberá utilizarse la siguiente ruta. USUARIO debe ser reemplazado por el nombre de usuario de Pythonanywhere
-RUTA_DESTINO = '/home/lospokes/mysite/static/imagenes'
+RUTA_DESTINO = '/home/gonzasana/mysite/static/imagenes'
 
 
 #--------------------------------------------------------------------
@@ -163,7 +163,7 @@ def mostrar_producto(codigo):
 def agregar_producto():
     #Recojo los datos del form
     descripcion = request.form['descripcion']
-    cantidad = request.form['cantidad']
+    Color = request.form['Color']
     precio = request.form['precio']
     imagen = request.files['imagen']
     proveedor = request.form['proveedor']  
@@ -175,7 +175,7 @@ def agregar_producto():
     nombre_base, extension = os.path.splitext(nombre_imagen) #Separa el nombre del archivo de su extensión.
     nombre_imagen = f"{nombre_base}_{int(time.time())}{extension}" #Genera un nuevo nombre para la imagen usando un timestamp, para evitar sobreescrituras y conflictos de nombres.
 
-    nuevo_codigo = catalogo.agregar_producto(descripcion, cantidad, precio, nombre_imagen, proveedor)
+    nuevo_codigo = catalogo.agregar_producto(descripcion, Color, precio, nombre_imagen, proveedor)
     if nuevo_codigo:    
         imagen.save(os.path.join(RUTA_DESTINO, nombre_imagen))
 
@@ -195,7 +195,7 @@ def agregar_producto():
 def modificar_producto(codigo):
     #Se recuperan los nuevos datos del formulario
     nueva_descripcion = request.form.get("descripcion")
-    nueva_cantidad = request.form.get("cantidad")
+    nueva_Color = request.form.get("Color")
     nuevo_precio = request.form.get("precio")
     nuevo_proveedor = request.form.get("proveedor")
     
@@ -230,7 +230,7 @@ def modificar_producto(codigo):
 
 
     # Se llama al método modificar_producto pasando el codigo del producto y los nuevos datos.
-    if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_cantidad, nuevo_precio, nombre_imagen, nuevo_proveedor):
+    if catalogo.modificar_producto(codigo, nueva_descripcion, nueva_Color, nuevo_precio, nombre_imagen, nuevo_proveedor):
         
         #Si la actualización es exitosa, se devuelve una respuesta JSON con un mensaje de éxito y un código de estado HTTP 200 (OK).
         return jsonify({"mensaje": "Producto modificado"}), 200
